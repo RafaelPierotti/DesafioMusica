9a5611c9823aa309938b3b322feb5f7e0433b98f
RafaelPierotti <rafaelpierotti236@gmail.com>
2025-01-03 18:00:25 -0300

    Enum criado

---
+++ src/main/java/Models/TipoArtista.java
@@ -0,0 +1,13 @@
+package Models;
+
+public enum TipoArtista {
+    SOLO("solo"),
+    DUPLA("dupla"),
+    BANDA("banda");
+
+    private final String tipo;
+
+    TipoArtista(String tipo){
+        this.tipo = tipo;
+    }
+}
---
+++ src/main/java/Models/Usuario.java
@@ -0,0 +1,16 @@
+package Models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private TipoArtista tipoArtista;
+}
